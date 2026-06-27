da6346327b4aaabfdd146fc87a60f5975cc6c5fb
webdevred <148627186+webdevred@users.noreply.github.com>
2026-06-27 16:21:56 +0200

    Added release auto-creation script

---
+++ .github/scripts/create_head_release.sh
@@ -0,0 +1,7 @@
+files_changed=$(git --no-pager diff --name-only main)
+
+if [[ echo "$files_changed" | grep -E '^(action.yml)|(setup-get-tested/action.yml)$' ]]; then
+    git tag -d get-tested-head-tmp
+    git tag get-tested-head-tmp
+    git push -f get-tested-head-tmp
+fi
