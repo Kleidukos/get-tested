adae25ad8784c0c3338ed04889be268e7a1cb208
webdevred <148627186+webdevred@users.noreply.github.com>
2026-06-27 16:50:23 +0200

    Fix

--- .github/scripts/create_head_release.sh
+++ .github/scripts/create_head_release.sh
@@ -2,7 +2,7 @@ git fetch
 
 files_changed=$(git --no-pager diff --name-only origin/main)
 
-if [[ echo "$files_changed" | grep -E '^(action.yml)|(setup-get-tested/action.yml)$' ]]; then
+if echo "$files_changed" | grep -E '^(action.yml)|(setup-get-tested/action.yml)$'; then
     git tag -d get-tested-head-tmp
     git tag get-tested-head-tmp
     git push -f get-tested-head-tmp
