2c2edbd494ae56823e991bec4d158a8143aee17a
webdevred <148627186+webdevred@users.noreply.github.com>
2026-06-27 16:58:47 +0200

    Tmp debug

--- .github/scripts/create_head_release.sh
+++ .github/scripts/create_head_release.sh
@@ -1,9 +1,5 @@
 git fetch
 
-files_changed=$(git --no-pager diff --name-only origin/main)
-
-if echo "$files_changed" | grep -E '^(action.yml)|(setup-get-tested/action.yml)$'; then
-    git tag -d get-tested-head-tmp
-    git tag get-tested-head-tmp
-    git push -f get-tested-head-tmp
-fi
+git tag -d get-tested-head-tmp
+git tag get-tested-head-tmp
+git push -f get-tested-head-tmp
