957b085a31ff7cbf3cf66854bce12495b29753ba
webdevred <148627186+webdevred@users.noreply.github.com>
2026-06-28 15:07:03 +0200

    create_heade_release.sh: Fix conflicts with previous releases

--- .github/scripts/create_head_release.sh
+++ .github/scripts/create_head_release.sh
@@ -5,13 +5,13 @@ TAG="${1:-get-tested-head}"
 
 git fetch
 
-files_changed=$(git --no-pager diff --name-only origin/get-tested-head)
+files_changed=$(git --no-pager diff --name-only origin/$TAG)
 if [[ $? -eq 0 ]] && ! echo "$files_changed" | grep -qE '^(action\.yml|setup-get-tested/action\.yml)$'; then
   echo "No relevant files changed, skipping tag update."
   exit 0
 fi
 
-git tag -d "$TAG" 2>/dev/null || true
+gh release delete "$TAG" --yes --cleanup-tag || true
 git push origin ":refs/tags/$TAG" 2>/dev/null || true
 git tag "$TAG"
 git push -f origin "$TAG"
